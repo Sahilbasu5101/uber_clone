de25644c91267e8bfe1766906dd1ec9e7a5e2241
sahilbasu5101 <sahilbasu5101@gmail.com>
2026-05-07 22:58:43 +0530

    frontend-initiated

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route , Routes } from  'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainSignup from './pages/CaptainSignup'
+import Captainlogin from './pages/Captainlogin'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route path="/captain-login" element={<Captainlogin />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
